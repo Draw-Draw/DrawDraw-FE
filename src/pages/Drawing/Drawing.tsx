724555fbd856b594516b3c143c642e40c81b784e
seocylucky <seocylucky@github.com>
2024-03-29 09:56:11 +0900

    rename: 일기 작성 페이지 Drawing.tsx로 수정

---
+++ src/pages/Drawing/Drawing.tsx
@@ -0,0 +1,7 @@
+export const Drawing = () => {
+  return (
+    <div>
+      <h1>일기 작성페이지입니다</h1>
+    </div>
+  );
+};
--- src/pages/Register.tsx
+++
@@ -1,7 +0,0 @@
-export const Register = () => {
-  return (
-    <div>
-      <h1>일기 작성 페이지입니다.</h1>
-    </div>
-  );
-};
