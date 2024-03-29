b192489a3abeaba5adac2ef92d0a0f3625e40741
seocylucky <seocylucky@github.com>
2024-03-29 15:49:46 +0900

    feat: main 페이지 퍼블리싱 작업

---
+++ src/pages/Home/Home.style.ts
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+import Background from '../../assets/Background.png';
+
+export const StyledContainer = styled.div`
+  background-image: url(${Background});
+  background-size: cover;
+  height: 110vh;
+  text-align: center;
+`;
+
+export const StyledBoardContainer = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  top: -4%;
+`;
+
+export const StyledGreenBoard = styled.img`
+  width: 1100px;
+  position: relative;
+
+  @media (min-width: 1601px) and (max-width: 1920px) {
+    width: 1300px;
+  }
+`;
+
+export const StyledEmptySketchBook = styled.img`
+  width: 280px;
+
+  @media (min-width: 1601px) and (max-width: 1920px) {
+    width: 310px;
+  }
+`;
+
+export const StyledTodayDiaryGrid = styled.div`
+  position: absolute;
+  margin-top: 90px;
+  display: grid;
+  grid-template-columns: repeat(3, 300px);
+  grid-template-rows: repeat(2, 240px);
+  align-items: center;
+  justify-items: center;
+
+  @media (min-width: 1601px) and (max-width: 1920px) {
+    margin-top: 100px;
+    grid-template-columns: repeat(3, 330px);
+    grid-template-rows: repeat(2, 270px);
+  }
+`;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,26 @@
+import {
+  StyledBoardContainer,
+  StyledContainer,
+  StyledEmptySketchBook,
+  StyledGreenBoard,
+  StyledTodayDiaryGrid,
+} from './Home.style';
+import { Header } from '../../components/Header/Header';
+import GreenBoard from '../../assets/GreenBoard.svg';
+import EmptySektchBook from '../../assets/EmptySketchBook.svg';
+
 export const Home = () => {
   return (
-    <div>
-      <h1>메인 페이지입니다.</h1>
-    </div>
+    <StyledContainer>
+      <Header />
+      <StyledBoardContainer>
+        <StyledGreenBoard src={GreenBoard} />
+        <StyledTodayDiaryGrid>
+          {Array.from({ length: 6 }).map((_, index) => (
+            <StyledEmptySketchBook src={EmptySektchBook} key={index} />
+          ))}
+        </StyledTodayDiaryGrid>
+      </StyledBoardContainer>
+    </StyledContainer>
   );
 };
