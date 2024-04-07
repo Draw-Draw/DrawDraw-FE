04cb79357ca3a3a6eecd472c46b1e6f91b5b8d61
seocylucky <chaeyeon2285@naver.com>
2024-04-07 23:48:50 +0900

    feat: 남의 일기장 상세 페이지 퍼블리싱

---
+++ src/components/NotMineDetailDiary/NotMineDetailDiary.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react';
+import { CommonModal } from '../Modal/CommonModal';
+import {
+  StyledBookMarked,
+  StyledBookMarkedContainer,
+  StyledContainer,
+  StyledDrawingBook,
+  StyledDrawingContainer,
+} from './NotMineDetailDiary.style';
+import DrawingBook from '../../assets/DrawingEmptySketchBook.png';
+import PutBookMark from '../../assets/bookmarks/PutBookMark.svg';
+import ShareBookMark from '../../assets/bookmarks/ShareBookMark.svg';
+// import SaveBookMark from '../../assets/bookmarks/SaveBookMark.svg';
+import { useSetModalType } from '../../hook/useSetModalType';
+
+export const NotMineDetailDiary = () => {
+  const [isOpenShareModal, setIsOpenShareModal] = useState(false);
+  const setModalType = useSetModalType();
+
+  useEffect(() => {
+    setIsOpenShareModal(false);
+    setModalType('SHARE');
+  }, []);
+
+  const handleShareOpenModal = () => {
+    setIsOpenShareModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsOpenShareModal(false);
+  };
+
+  return (
+    <StyledContainer>
+      <StyledDrawingContainer>
+        <StyledDrawingBook src={DrawingBook} />
+        <StyledBookMarkedContainer>
+          <StyledBookMarked src={PutBookMark} />
+          <StyledBookMarked src={ShareBookMark} onClick={handleShareOpenModal} />
+          {/* <StyledBookMarked src={SaveBookMark} /> */}
+        </StyledBookMarkedContainer>
+      </StyledDrawingContainer>
+      {isOpenShareModal && <CommonModal onCloseModal={() => handleCloseModal()} />}
+    </StyledContainer>
+  );
+};
