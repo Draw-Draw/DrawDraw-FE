f7fde5494b5df0fd5fb7aec485c3d69536df0bbb
seocylucky <seocylucky@github.com>
2024-04-07 12:26:35 +0900

    feat: 나의 일기장 페이지 작업

---
+++ src/pages/UserDiaryBoard/UserDiaryBoard.tsx
@@ -0,0 +1,28 @@
+// import { useParams } from 'react-router-dom';
+import { Header } from '../../components/Header/Header';
+import {
+  StyledBoardContainer,
+  StyledContainer,
+  StyledEmptySketchBook,
+  StyledGreenBoard,
+  StyledTodayDiaryGrid,
+} from './UserDiaryBoard.style';
+import EmptySektchBook from '../../assets/EmptySketchBook.png';
+import GreenBoard from '../../assets/MyGreenBoard.svg';
+
+export const UserDiaryBoard = () => {
+  // const { id } = useParams();
+  return (
+    <StyledContainer>
+      <Header isDrawing={false} />
+      <StyledBoardContainer>
+        <StyledGreenBoard src={GreenBoard} />
+        <StyledTodayDiaryGrid>
+          {Array.from({ length: 6 }).map((_, index) => (
+            <StyledEmptySketchBook src={EmptySektchBook} key={index} />
+          ))}
+        </StyledTodayDiaryGrid>
+      </StyledBoardContainer>
+    </StyledContainer>
+  );
+};
