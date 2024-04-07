0f8823b5b706460278f64cb9d3b61bd63f19f954
seocylucky <chaeyeon2285@naver.com>
2024-04-07 23:43:50 +0900

    feat: 나의 일기 상세페이지 퍼블리싱

---
+++ src/components/MyDetailDiary/MyDetailDiary.style.ts
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+export const StyledContainer = styled.div`
+  text-align: center;
+`;
+
+export const StyledDrawingContainer = styled.div`
+  width: 68vw;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 auto;
+  top: -8%;
+`;
+
+export const StyledDrawingBook = styled.img`
+  position: absolute;
+  width: 68vw;
+  z-index: 1;
+`;
+
+export const StyledBookMarkedContainer = styled.div`
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  margin-left: 100%;
+  margin-top: 11%;
+`;
+
+export const StyledBookMarked = styled.img`
+  width: 190px;
+  cursor: pointer;
+  &:hover {
+    margin-left: 10%;
+  }
+`;
