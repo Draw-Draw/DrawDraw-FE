0f806508a6dea14aa1badf90749fde5c6eeafcb3
seocylucky <chaeyeon2285@naver.com>
2024-04-28 18:15:06 +0900

    feat: WriteOrView 모달 작업

---
+++ src/components/Modal/ModalType/WriteOrView/WriteOrView.style.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const StyledModalTextContainer = styled.div`
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  gap: 27px;
+  align-items: center;
+  z-index: 9999;
+`;
+
+export const StyledTitleText = styled.div`
+  font-family: SSMullaeler;
+  z-index: 9999;
+  color: #511f05;
+  font-size: 45px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 78.766px;
+`;
+
+export const StyledBtnContainer = styled.div`
+  margin-top: 40px;
+  display: flex;
+  flex-direction: row;
+  gap: 80px;
+`;
+
+export const StyledBtn = styled.img`
+  width: 110px;
+  cursor: pointer;
+`;
---
+++ src/components/Modal/ModalType/WriteOrView/WriteOrView.tsx
@@ -0,0 +1,42 @@
+import {
+  StyledBtn,
+  StyledBtnContainer,
+  StyledModalTextContainer,
+  StyledTitleText,
+} from './WriteOrView.style';
+import WriteBtn from '../../../../assets/buttons/WriteBtn.svg';
+import ViewBtn from '../../../../assets/buttons/ViewBtn.svg';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+
+export const WriteOrView = ({
+  diaryName,
+  diarybookId,
+}: {
+  diaryName: string | undefined;
+  diarybookId: number | undefined;
+}) => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    console.log(diaryName);
+  }, []);
+
+  const handleWriteBtnClick = () => {
+    navigate(`/drawing/${diarybookId}`);
+  };
+
+  const handleViewBtnClick = () => {
+    navigate(`/diary/${diarybookId}`);
+  };
+
+  return (
+    <StyledModalTextContainer>
+      <StyledTitleText>"{diaryName}"</StyledTitleText>
+      <StyledBtnContainer>
+        <StyledBtn src={WriteBtn} onClick={handleWriteBtnClick} />
+        <StyledBtn src={ViewBtn} onClick={handleViewBtnClick} />
+      </StyledBtnContainer>
+    </StyledModalTextContainer>
+  );
+};
