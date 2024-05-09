62792d63da80803960d7951fd2c5cdf5d29b8c2a
seocylucky <seocylucky@github.com>
2024-05-10 02:58:48 +0900

    feat: 공유용 모바일 뷰 작업

--- src/pages/MobileView/MobileView.style.ts
+++ src/pages/MobileView/MobileView.style.ts
@@ -8,7 +8,131 @@ export const StyledContainer = styled.div`
   background-color: #f5f5f5;
   display: flex;
   flex-direction: column;
+  background-color: #d8cdaf;
   @media screen and (max-width: 768px) {
     width: 100%;
   }
+  overflow-x: hidden;
+`;
+
+export const StyledMobileContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const StyledHeader = styled.div`
+  width: 100%;
+  height: 54px;
+  border-top: 4.5px solid #672909;
+  display: flex;
+  flex-direction: row;
+  gap: 29px;
+  margin-top: 70px;
+  color: #672909;
+  font-family: SSMullaeler;
+  font-size: 22.54px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 38.318px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const StyledLine = styled.div`
+  width: 4px;
+  height: inherit;
+  background-color: #672909;
+`;
+
+export const StyledDayContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+  align-items: center;
+`;
+
+interface IconProps {
+  type: string;
+}
+
+export const StyledIcon = styled.img<IconProps>`
+  z-index: 10000;
+  width: ${(props) =>
+    props.type === 'SUNNY'
+      ? '57px'
+      : props.type === 'CLOUDY'
+        ? '75px'
+        : props.type === 'MOON'
+          ? '52px'
+          : props.type === 'RAINBOW'
+            ? '92px'
+            : props.type === 'RAINY'
+              ? '72px'
+              : props.type === 'SNOWY'
+                ? '33px'
+                : 'inherit'};
+
+  top: 0%;
+  left: 58%;
+  @media (min-width: 1601px) and (max-width: 1920px) {
+    top: 16%;
+    left: 58%;
+  }
+  @media (max-width: 1366px) {
+    font-size: 30px;
+    top: 10%;
+    left: 58%;
+    width: ${(props) =>
+      props.type === 'SUNNY'
+        ? '75px'
+        : props.type === 'CLOUDY'
+          ? '58px'
+          : props.type === 'MOON'
+            ? '35px'
+            : props.type === 'RAINBOW'
+              ? '75px'
+              : props.type === 'RAINY'
+                ? '55px'
+                : props.type === 'SNOWY'
+                  ? '16px'
+                  : 'inherit'};
+  }
+`;
+
+export const StyledImg = styled.img`
+  background-size: cover;
+  height: 257px;
+  border-top: 4.5px solid #672909;
+  border-bottom: 4.5px solid #672909;
+`;
+
+export const StyledText = styled.div`
+  z-index: 3;
+  margin-top: 21px;
+  padding-left: 30px;
+  padding-right: 30px;
+  color: #672909;
+  font-family: SSMullaeler;
+  font-size: 28px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 43px;
+  letter-spacing: 10px;
+  word-spacing: 4px;
+`;
+
+export const StyledStamp = styled.img`
+  position: relative;
+  bottom: 180px;
+  left: 110px;
+  width: 133px;
+  opacity: 0.57;
+  z-index: 2;
+`;
+
+export const StyledLogo = styled.img`
+  width: 130px;
+  margin-top: 8px;
+  /* margin-bottom: 70px; */
 `;
--- src/pages/MobileView/MobileView.tsx
+++ src/pages/MobileView/MobileView.tsx
@@ -1,5 +1,111 @@
-import { StyledContainer } from './MobileView.style';
+import { useEffect, useState } from 'react';
+import {
+  StyledContainer,
+  StyledDayContainer,
+  StyledHeader,
+  StyledIcon,
+  StyledImg,
+  StyledLine,
+  StyledLogo,
+  StyledMobileContainer,
+  StyledStamp,
+  StyledText,
+} from './MobileView.style';
+import { useParams } from 'react-router-dom';
+import { getDiary } from '../../apis/getDiary';
+import { ResultDiaryType } from '../../types/ResultDiary.type';
+
+import Sunny from '../../assets/weathers/Sunny.png';
+import Cloud from '../../assets/weathers/Cloud.png';
+import Moon from '../../assets/weathers/Moon.png';
+import Rainbow from '../../assets/weathers/Rainbow.png';
+import Rainy from '../../assets/weathers/Rainy.png';
+import Snow from '../../assets/weathers/Snow.png';
+import Stamp from '../../assets/Stamps/GoodStamp.png';
+import Logo from '../../assets/Logo.png';
 
 export const MobileView = () => {
-  return <StyledContainer>모바일 뷰 화면</StyledContainer>;
+  const { diarybookid, diaryid } = useParams<{
+    diarybookid: string | undefined;
+    diaryid: string | undefined;
+  }>();
+  const [diaryData, setDiaryData] = useState<ResultDiaryType | null>(null);
+
+  useEffect(() => {
+    if (diarybookid && diaryid) {
+      const fetchDiary = async () => {
+        try {
+          const data = await getDiary(diarybookid, diaryid);
+          setDiaryData(data);
+        } catch (error) {
+          console.error('Error fetching diary:', error);
+        }
+      };
+      fetchDiary();
+    }
+  }, [diarybookid, diaryid]);
+
+  const dateString = diaryData?.date;
+
+  let year: number | undefined;
+  let month: number | undefined;
+  let day: number | undefined;
+
+  if (dateString) {
+    const [yearStr, monthStr, dayStr] = dateString.split('-');
+    year = parseInt(yearStr, 10);
+    month = parseInt(monthStr, 10);
+    day = parseInt(dayStr, 10);
+  }
+
+  return (
+    <StyledContainer>
+      <StyledMobileContainer>
+        <StyledHeader>
+          <div>
+            {year}. {String(month).padStart(2, '0')}. {String(day).padStart(2, '0')}
+          </div>
+          <StyledLine />
+          <StyledDayContainer>
+            {diaryData?.weather &&
+              (diaryData?.weather === 'SUNNY' ? (
+                <div>해가 쨍쨍</div>
+              ) : diaryData?.weather === 'CLOUDY' ? (
+                <div>구름이 많아요</div>
+              ) : diaryData?.weather === 'MOON' ? (
+                <div>별이 빛나는 밤에</div>
+              ) : diaryData?.weather === 'RAINBOW' ? (
+                <div>일곱빛깔 무지개</div>
+              ) : diaryData?.weather === 'RAINY' ? (
+                <div>비가 주륵주륵</div>
+              ) : (
+                <div>눈이 평평</div>
+              ))}
+            {diaryData?.weather && (
+              <StyledIcon
+                type={diaryData?.weather}
+                src={
+                  diaryData?.weather === 'SUNNY'
+                    ? Sunny
+                    : diaryData?.weather === 'CLOUDY'
+                      ? Cloud
+                      : diaryData?.weather === 'MOON'
+                        ? Moon
+                        : diaryData?.weather === 'RAINBOW'
+                          ? Rainbow
+                          : diaryData?.weather === 'RAINY'
+                            ? Rainy
+                            : Snow
+                }
+              />
+            )}
+          </StyledDayContainer>
+        </StyledHeader>
+        <StyledImg src={diaryData?.imageUrl} />
+        <StyledText>{diaryData?.content}</StyledText>
+        <StyledLogo src={Logo} />
+        <StyledStamp src={Stamp} />
+      </StyledMobileContainer>
+    </StyledContainer>
+  );
 };
