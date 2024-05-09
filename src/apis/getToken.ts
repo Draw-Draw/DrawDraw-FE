614218d936c30f23a738c449dddbc6ccd7837f8e
seocylucky <seocylucky@github.com>
2024-05-10 03:29:56 +0900

    fix: response.data -> response.data.data.accessToken

--- src/apis/getToken.ts
+++ src/apis/getToken.ts
@@ -3,6 +3,6 @@ import { client } from './client';
 export const getToken = async () => {
   const response = await client.get(`/api/v1/auth`);
 
-  localStorage.setItem('accessToken', response.data);
+  localStorage.setItem('accessToken', response.data.data.accessToken);
   return response.data.data.accessToken;
 };
