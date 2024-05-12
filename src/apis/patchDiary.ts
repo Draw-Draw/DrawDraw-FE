1e49b0cdfa72a2702fc356ddd7da5120d3d504c6
seocylucky <chaeyeon2285@naver.com>
2024-05-13 03:34:41 +0900

    feat: 수정 성공 시 alert 창 띄우는 작업

--- src/apis/patchDiary.ts
+++ src/apis/patchDiary.ts
@@ -7,19 +7,23 @@ export const patchDiary = async (
   { weather, content }: ResultDiaryType
 ) => {
   const accessToken = localStorage.getItem('accessToken');
-  const response = await client.patch(
-    `/api/v1/diaries/diary-book/${diaryBookId}/diary/${diaryId}`,
-    {
-      weather,
-      content,
-    },
-    {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
+  try {
+    const response = await client.patch(
+      `/api/v1/diaries/diary-book/${diaryBookId}/diary/${diaryId}`,
+      {
+        weather,
+        content,
       },
-    }
-  );
-
-  console.log(response.data);
-  return response.data.data;
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    alert('수정이 완료되었습니다.');
+    return response.data.data;
+  } catch (error) {
+    console.error('Error updating diary:', error);
+    throw error;
+  }
 };
