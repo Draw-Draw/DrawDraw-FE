100ac763ad5a3e2d02180351c60ab6e51b9d6a38
seocylucky <chaeyeon2285@naver.com>
2024-05-16 00:39:05 +0900

    refactor: 불필요한 코드 정리

--- src/apis/client.ts
+++ src/apis/client.ts
@@ -4,22 +4,6 @@ export const client = axios.create({
   baseURL: process.env.REACT_APP_BASEURL,
 });
 
-// client.interceptors.request.use(
-//   async (config) => {
-//     const response = await axios.get('http://13.125.42.202:8080/api/v1/auth');
-//     const token = response.data.data.accessToken;
-
-//     console.log(token);
-
-//     config.headers.Authorization = `Bearer ${token}`;
-
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
-
 client.interceptors.response.use(
   (response) => {
     console.log(response);
